8e2986f0e697571579177bee6994cf1982e44148
DanielMena4 <129901636+DanielMena4@users.noreply.github.com>
2023-10-15 18:09:17 -0500

    Update factura.c

--- factura.c
+++ factura.c
@@ -1,62 +1,94 @@
-#include <stdio.h>
-#include <string.h>
-int ncedula(){
-int cedula, ceduaux , aux , aux2 ,snum , par;
-aux = 0;
-par = 0;
-    while (snum != aux ) {
-        printf("\nIngrese la cedula/RUC: ");
-        scanf("%d", &cedula);
-        ceduaux = cedula;
-        snum = cedula % 10;
-        cedula = cedula / 10;
-            while(cedula != 0)
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int validacion(char cedula[11])
+{
+    for(int i = 0; i < strlen(cedula);i++)
+    {
+        if(!(isdigit(cedula[i])))
         {
-            if(par==0)
+        printf("\nNumero no valido");
+        return 0;
+        }
+    }
+    return 1;    
+}
+
+int cedulan(char cedula[11])
+{
+    int aux,aux2,snum, par;
+    aux = atoi(cedula);
+    par = 0;
+    snum = aux % 10;
+    aux = aux / 10;
+            while(aux != 0)
         {
-            if(2*(cedula % 10)>10)
+                if(par == 0)
             {
-                aux2 = 2*(cedula%10);
-                aux+= aux2%10;
-                aux2 = aux2 / 10;
-                aux+= aux2%10;
+                if(2*(aux % 10)>10)
+                {
+                    aux2 = 2*(aux%10);
+                    aux+= aux2%10;
+                    aux2 = aux2 / 10;
+                    aux+= aux2%10;
+                }
+                    else
+                {
+                    aux+= 2 * (aux % 10); 
+                }
+                aux = aux /10;
+                par = 1;
             }
             else
             {
-                aux+= 2 * (cedula % 10); 
+                aux+= aux%10;
+                par = 0;
+                aux= aux/10;
             }
-            cedula = cedula /10;
-            par = 1;
         }
-        else
-        {
-            aux+= cedula%10;
-            par = 0;
-            cedula= cedula/10;
-        }
+    aux = 10 - (aux % 10);
+    if(snum == aux)
+    {
+        return 1;
     }
-            aux = (aux%10);
-            aux = 10 - (aux % 10);
-        if (snum != aux) {
-            printf("\n Numero de cedula invalido");
-        }
+    else
+    {
+        return 0;
     }
-    return (ceduaux);
 }
+
+int ceduruc(char cedula[11])
+{
+int num;
+    num = validacion(cedula);
+    if(num == 0)
+    {
+        return 0;
+    }
+    num = cedulan(cedula);
+    return 1;
+}
+
 int main(){
-    int menu, ceduaux, aux, subtotal, cuent_a, cuent_b,cuent_c, cuent_d, cuent_e;
-    cuent_e = 0;
-    cuent_b = 0;
-    cuent_a = 0;
-    cuent_c = 0;
-    cuent_d = 0;
+    int menu, menu2, menu3, cuent[5], cedula;
+    float subtotal;
+    cuent[0] = 0;
+    cuent[1] = 0;
+    cuent[2] = 0;
+    cuent[3] = 0;
+    cuent[4] = 0;
+    cedula = 0;
     menu = 0;
     subtotal = 0;
     char nombre[20] = "Consumidor";
     char apellido[20] = "Final";
     char ubicacion[20] = " ";
-    char telefono[20] = "000000000";
-    char producto[10];
+    char ceduaux[11] = "000000000";
+    char telefono[11] = "000000000";
+    char producto[2];
     while (menu != 2) {
         printf("1. Ingresar datos \n2. Consumidor final \n");
         scanf("%d", &menu);
@@ -67,11 +99,15 @@ int main(){
             scanf("%s",nombre);
             printf("\nIngrese el apellido: ");
             scanf("%s",apellido);
-            ceduaux = ncedula();
+            while(cedula == 0){
+                printf("\nIngrese cedula/RUC: ");
+                scanf("%s", ceduaux);
+                cedula = ceduruc(ceduaux);    
+            }
             printf("\nIngrese la ubicacion: ");
             scanf("%s", ubicacion);
             printf("\nIngrese el telefono: ");
-            scanf(telefono,20,stdin);
+            scanf("%s",telefono);
             menu = 2;
         break;
     case 2:
@@ -89,52 +125,49 @@ int main(){
         switch (menu)
         {
             case 1:
-            aux = 1;
-            while (aux != 0) {
-                printf("\na. llantas($150)\nb. kit de pastillas de freno($55)\nc. kit de embrague($180)\nd. Faro($70)\ne. radiador($120)\ng. Salir");
+            menu2 = 1;
+            while (menu2 != 0) {
+                printf("\na. llantas($150)\nb. kit de pastillas de freno($55)\nc. kit de embrague($180)\nd. Faro($70)\ne. radiador($120)\nf. Salir");
                 printf("\nIngrese el producto: ");
                 scanf("%s",producto);
-                if (strcmp(producto,"a")==0) {
-                    printf("\n Ingrese la cantidad del producto comprado: ");
-                    scanf("%d", &cuent_a);
-                    subtotal+= 150 * cuent_a;
+                if (strcmp(producto,"a")==0) 
+                {
+                    printf("\nIngrese la cantidad del producto comprado: ");
+                    scanf("%d", &cuent[0]);
+                    subtotal+= 150 * cuent[0];
                 }
                 else if (strcmp(producto,"b")==0)
                 {
-                    printf("\n Ingrese la cantidad del producto comprado: ");
-                    scanf("%d", &cuent_b);
-                    subtotal+= 55 * cuent_b;
-                    cuent_b++;
+                    printf("\nIngrese la cantidad del producto comprado: ");
+                    scanf("%d", &cuent[1]);
+                    subtotal+= 55 * cuent[1];
                 }
                 else if (strcmp(producto,"c")==0)
                 {
-                    printf("\n Ingrese la cantidad del producto comprado: ");
-                    scanf("%d", &cuent_c);
-                    subtotal+= 180 * cuent_c;
-                    cuent_c++;
+                    printf("\nIngrese la cantidad del producto comprado: ");
+                    scanf("%d", &cuent[2]);
+                    subtotal+= 180 * cuent[2];
                 }
                 else if (strcmp(producto,"d")==0)
                 {
-                    printf("\n Ingrese la cantidad del producto comprado: ");
-                    scanf("%d", &cuent_d);
-                    subtotal+= 70 * cuent_d;
-                    cuent_d++;
+                    printf("\nIngrese la cantidad del producto comprado: ");
+                    scanf("%d", &cuent[3]);
+                    subtotal+= 70 * cuent[3];
                 }
                 else if (strcmp(producto,"e")==0)
                 {
-                    printf("\n Ingrese la cantidad del producto comprado: ");
-                    scanf("%d", &cuent_e);
-                    subtotal+= 120 * cuent_e;
-                    cuent_e++;
-                }else if (strcmp(producto,"g")==0)
+                    printf("\nIngrese la cantidad del producto comprado: ");
+                    scanf("%d", &cuent[4]);
+                    subtotal+= 120 * cuent[4];
+
+                }else if (strcmp(producto,"f")==0)
                 {
-                    aux = 0;
+                    menu2 = 0;
                 }
                 else {
-                    printf("\n Ingrese un producto valido");
+                    printf("\nIngrese un producto valido");
                 }
             }
-            
             break;
             case 2:
             break;
@@ -143,44 +176,47 @@ int main(){
             break;
         }
     }
-    printf("\n %s , %s , %d , %s, %s ", nombre, apellido, ceduaux, ubicacion, telefono);
-    printf("\n\t Producto \t Valor unitario \t cantidad \t Valor combinado");
-    if (cuent_a != 0)
+    printf("\nNombre: %s\nApellido: %s\nCedula/RUC: %s\nUbicacion: %s\nTelefono: %s ", nombre, apellido, ceduaux, ubicacion, telefono);
+    if (cuent[0] != 0)
     {
-    printf("\n\tllantas \t  $150 \t  \t %d \t %d", cuent_a , (cuent_a*150));
+    printf("\n\tProducto: llantas\tValor unitario: $150\tCantidad: %d\tValor combinado: $%d", cuent[0] , (cuent[0]*150));
     }
-        if (cuent_b != 0)
+        if (cuent[1] != 0)
     {
-    printf("\n\tkit de pastillas de freno  $55  \t\t %d \t %d", cuent_b , (cuent_b*55));
+    printf("\n\tProducto: kit de pastillas de freno\tValor unitario: $55\tCantidad: %d\tValor combinado: $%d", cuent[1] , (cuent[1]*55));
     }
-        if (cuent_c != 0)
+        if (cuent[2] != 0)
     {
-    printf("\n\tkit de embrague \t \t $180  \t %d \t %d", cuent_c , (cuent_c*180));
+    printf("\n\tProducto: kit de embrague\tValor unitario: $180\tCantidad: %d\tValor combinado: $%d", cuent[2] , (cuent[2]*180));
     }
-        if (cuent_d != 0)
+        if (cuent[3] != 0)
     {
-    printf("\n\tfaro \t \t \t  $70  \t %d \t %d", cuent_d , (cuent_d*70));
+    printf("\n\tProducto: faro\tValor unitario: $70\tCantidad: %d\tValor combinado: $%d", cuent[3] , (cuent[3]*70));
     }
-        if (cuent_e != 0)
+        if (cuent[4] != 0)
     {
-    printf("\n\tradiador \t  $120  \t \t %d \t %d", cuent_d , (cuent_d*120));
+    printf("\n\tProducto: radiador\tValor unitario: $120\tCantidad: %d\tValor combinado: $%d", cuent[4] , (cuent[4]*120));
     }
-    printf("subtotal: %d", subtotal);
+    printf("\nSubtotal: $%.2f", subtotal);
     if (subtotal > 100 && subtotal <501)
     {
-        subtotal = subtotal * 0,5;
+        printf("\nDescuento 5: $%.2f ", subtotal*0.05);
+        subtotal-= (subtotal * 0.05);
     }
         if (subtotal > 500 && subtotal <1000)
     {
-        subtotal = subtotal * 0,7;
+        printf("\nDescuento 7: $%.2f ", subtotal*0.07);
+        subtotal-=(subtotal * 0.07);
     }
         if (subtotal >1000)
     {
-        subtotal = subtotal * 0,10;
+        printf("\nDescuento 10: $%.2f ", subtotal * 0.1);
+        subtotal-=(subtotal * 0.1);
     }
+
+    printf("\nIva 12: $%.2f", subtotal*0.12);
     subtotal = subtotal * 1.12;
-    printf("Iva 12 %d", subtotal * 0.12);
-    printf("Total: %d", subtotal);
+    printf("\nTotal: $%.2f", subtotal);
     
 
     return 0;
